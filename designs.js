d7ea4159aed4bfd47ce6d5cb802bed535190e4b4
c mulligan <gateless@protonmail.com>
2018-02-28 01:58:47 -0500

    add style and fix blackout

--- designs.js
+++ designs.js
@@ -5,6 +5,8 @@ var gridStuff = {
   rows: 1
 };
 
+pixelCanvas = $('#pixelCanvas')
+
 $('#colorPicker').on('input',function (event) {
   gridStuff.color = $(event.target).val();
 });
@@ -13,19 +15,20 @@ $('#colorPicker').on('input',function (event) {
 $('#submit').on('click',function () {
   gridStuff.cols = $('#inputWidth').val();
   gridStuff.rows = $('#inputHeight').val();
-  if (gridStuff.cols <=30 && gridStuff.rows <=30) {
+  if (gridStuff.cols <=100 && gridStuff.rows <=100) {
     $('#marquee').hide();
-    $('#pixelCanvas').children().remove();
-    $('button').text('start over');
+    pixelCanvas.children().remove();
+    $('#submit').text('start over');
     makeGrid(gridStuff.cols,gridStuff.rows);
-}
+    $('#grid').addClass('showGrid');
+  }
 });
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid(c,r) {
   for (x=1;x<=r;x++) {
-    $('#pixelCanvas').append(
+    pixelCanvas.append(
       "<tr class='xCord'></tr>"
     );
   }
@@ -36,7 +39,7 @@ function makeGrid(c,r) {
 
 var paint = false;
 
-$('#pixelCanvas').mousedown(function (event) {
+pixelCanvas.mousedown(function (event) {
   event.preventDefault();
   paint = true;
 });
@@ -45,8 +48,14 @@ $('body').mouseup(function () {
   paint = false;
 });
 
-$('#pixelCanvas').mouseover('td',function (event) {
+pixelCanvas.mouseleave(function () {
+  paint = false;
+});
+
+pixelCanvas.mouseover('td',function (event) {
+  event.stopPropagation();
   if (paint) {
+    console.log(event.target);
     $(event.target).css('background-color',gridStuff.color);
   }
 });
