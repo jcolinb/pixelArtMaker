87e43a377b4265c8c2b0bfeaf844863a0efb5e27
c mulligan <gateless@protonmail.com>
2018-02-28 02:13:02 -0500

    update gh-pages

--- designs.js
+++ designs.js
@@ -15,14 +15,6 @@ $('#colorPicker').on('input',function (event) {
 $('#submit').on('click',function () {
   gridStuff.cols = $('#inputWidth').val();
   gridStuff.rows = $('#inputHeight').val();
-<<<<<<< HEAD
-  if (gridStuff.cols <=30 && gridStuff.rows <=30) {
-    $('#marquee').hide();
-    $('#pixelCanvas').children().remove();
-    $('button').text('start over');
-    makeGrid(gridStuff.cols,gridStuff.rows);
-}
-=======
   if (gridStuff.cols <=100 && gridStuff.rows <=100) {
     $('#marquee').hide();
     pixelCanvas.children().remove();
@@ -30,7 +22,6 @@ $('#submit').on('click',function () {
     makeGrid(gridStuff.cols,gridStuff.rows);
     $('#grid').addClass('showGrid');
   }
->>>>>>> master
 });
 
 // When size is submitted by the user, call makeGrid()
@@ -48,11 +39,7 @@ function makeGrid(c,r) {
 
 var paint = false;
 
-<<<<<<< HEAD
-$('#pixelCanvas').mousedown(function (event) {
-=======
 pixelCanvas.mousedown(function (event) {
->>>>>>> master
   event.preventDefault();
   paint = true;
 });
@@ -61,10 +48,6 @@ $('body').mouseup(function () {
   paint = false;
 });
 
-<<<<<<< HEAD
-$('#pixelCanvas').mouseover('td',function (event) {
-  if (paint) {
-=======
 pixelCanvas.mouseleave(function () {
   paint = false;
 });
@@ -73,7 +56,6 @@ pixelCanvas.mouseover('td',function (event) {
   event.stopPropagation();
   if (paint) {
     console.log(event.target);
->>>>>>> master
     $(event.target).css('background-color',gridStuff.color);
   }
 });
